f6c5320dd1d7094f72b337e8f199d680dae363b8
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-17 18:21:49 +0200

    Add inputs validations modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -39,7 +39,24 @@ class _NewExpenseState extends State<NewExpense> {
     final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
     if (_titleController.text.trim().isEmpty ||
         amountIsInvalid ||
-        _selectedDate == null) {}
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text(
+              'Please make sure a valid title, amount, date and category was entered.'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(ctx);
+                },
+                child: const Text('Okay'))
+          ],
+        ),
+      );
+      return;
+    }
   }
 
   @override
@@ -125,7 +142,7 @@ class _NewExpenseState extends State<NewExpense> {
               ),
               ElevatedButton(
                 onPressed: () {
-                  _submitExpenseData;
+                  _submitExpenseData();
                 },
                 child: const Text('Save Expense'),
               )
